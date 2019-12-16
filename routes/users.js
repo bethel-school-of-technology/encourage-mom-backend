48e008f0de83281edb8800438bf6e4b0e1a5297e
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2019-12-16 11:31:16 -0800

    Added User.js model

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup')
+})
+
+router.post('/signup', function(req, res, next) {
+
+})
+
+router.get('/login', function(req, res, next) {
+  res.render('login')
+})
+
+router.post('/login', function(req, res, next) {
+
+})
 
 module.exports = router;
