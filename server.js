07c1fd5caf66f08ed04c0e46601d3428bb5877c6
Jason Bump <bump.jason@gmail.com>
2019-12-16 19:52:49 -0600

    first commit to jasons branch

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
