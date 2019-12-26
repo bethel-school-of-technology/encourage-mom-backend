67f957d2d8fa0c7087eb4c15079490e082724dff
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2019-12-26 12:01:01 -0800

    Fixing Updates and Merge conflicts

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const connectDB = require('./config/db');
-
-const app = express();
-
-// Connect Database
-connectDB();
-
-app.get('/', (req, res) => res.send('API Running'));
-
-// Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/post', require('./routes/api/post'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/auth', require('./routes/api/auth'));
-
-const PORT = process.env.PORT || 5000;
-
-=======
 const express = require('express');
 const connectDB = require('./config/db');
 
@@ -36,5 +16,4 @@ app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
->>>>>>> 3eccf8709be0925f6cfbf4994fd9d9d02ae7c26f
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
