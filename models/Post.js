b615b78e8bf841c58b7241457dbd576b771ec97d
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2019-12-27 08:27:46 -0800

    Fixed code so I can get it running!

--- models/Post.js
+++ models/Post.js
@@ -12,11 +12,11 @@ const PostSchema = new Schema ({
     },
     name: {
         stype: String,
-        required: true
+        // required: true
     },
-    avatar: {
-        type: String
-    },
+    // avatar: {
+    //     type: String
+    // },
     likes: [
         {
         user: {
