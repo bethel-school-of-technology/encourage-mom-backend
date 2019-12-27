10263459d2b9f0659c8919253ee9c979273dae9a
Vince Oblak <voblak2@gmail.com>
2019-12-26 20:33:40 -0600

    Updating branches so everything works

--- models/User.js
+++ models/User.js
@@ -2,35 +2,35 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     
-     UserId: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER
-    },
+    //  UserId: {
+    //     allowNull: false,
+    //     autoIncrement: true,
+    //     primaryKey: true,
+    //     type: DataTypes.INTEGER
+    // },
     FirstName: {
-        type: DataTypes.STRING,
+        type: String,
         unique: true
     },
     LastName: {
-        type: DataTypes.STRING,
+        type: String,
         unique: true,
     },
     Username: {
-        type: DataTypes.STRING,
+        type: String,
         unique: true,
         required: true,
     },
     Password: {
-        type: DataTypes.STRING,
+        type: String,
         required: true
     },
     Email: {
-        type: DataTypes.STRING,
+        type: String,
         unique: true,
     },
     createdAt: {
-        type: DataTypes.DATE,
+        type: Date,
         default: Date.now
 } 
 });
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const bcrypt= require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 //This Error thrown in Terminal so updated below: express-validator: requires to express-validator/check are deprecated.You should just use require("express-validator") instead.
 const { check, validation } = require('express-validator');
 
-const User = require('../models/User');
+const User = require('../../models/User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -14,37 +14,50 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/signup', function(req, res, next) {
-  res.render('signup')
-})
+  res.render('signup');
+});
 
-router.post('/signup', [
-  check('FirstName', 'First Name is required').not().isEmpty(),
-  check('LastName', 'Last Name is required').not().isEmpty(),
-  check('Email', 'Please include a valid email').not().isEmpty(),
-  check('Username', 'Username is Required').not().isEmpty(),
-  check('Password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-],
- async (req, res) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()) {
+router.post(
+  '/signup',
+  [
+    check('FirstName', 'First Name is required')
+      .not()
+      .isEmpty(),
+    check('LastName', 'Last Name is required')
+      .not()
+      .isEmpty(),
+    check('Email', 'Please include a valid email')
+      .not()
+      .isEmpty(),
+    check('Username', 'Username is Required')
+      .not()
+      .isEmpty(),
+    check(
+      'Password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const {FirstName, LastName, Email, Username, Password } = req.body;
+    const { FirstName, LastName, Email, Username, Password } = req.body;
 
     try {
-      let user = await User.findOne({ email })
+      let user = await User.findOne({ email });
 
-      if(user) {
-        res.status(400).json({ errors: [ { msg: 'User already exists'}] });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      user = new User ({
-          FirstName,
-          LastName,
-          Email,
-          Username,
-          Password
+      user = new User({
+        FirstName,
+        LastName,
+        Email,
+        Username,
+        Password
       });
 
       const salt = await bcrypt.genSalt(10);
@@ -57,33 +70,36 @@ router.post('/signup', [
         user: {
           id: user.id
         }
-      }
+      };
 
-      jwt.sign(
-        payload,
-        config.get('jwtSecret'),
-        {expiresIn: 360000}),
+      jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }),
         (err, token) => {
-          if(err) throw err;
+          if (err) throw err;
           res.json({ token });
-        }
-
-    } catch(err) {
+        };
+    } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
-      res
+      res;
     }
-})
+  }
+);
 
 router.get('/login', function(req, res, next) {
-  res.render('login')
-})
+  res.render('login');
+});
 
-router.post('/login', [
-  check('Username', 'Username is Required').not().isEmpty(),
-  check('Password', 'Password is Required').not().isEmpty(),
-], (req, res) => {
-
-})
+router.post(
+  '/login',
+  [
+    check('Username', 'Username is Required')
+      .not()
+      .isEmpty(),
+    check('Password', 'Password is Required')
+      .not()
+      .isEmpty()
+  ],
+  (req, res) => {}
+);
 
 module.exports = router;
