fa12b7ab7042ba97e139bf897643c25194e1a2b0
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2020-01-06 13:22:55 -0800

    Cleaned up file folder added Navbar component

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="nav bg-dark">
+        <ul>
+            <li><h1>Crown of Jewels</h1></li>
+            <li><a href="home.html">Home</a></li>
+            <li><a href="daily-encouragement.html">Daily Encouragent</a></li>
+            <li><a href="posts.html">Posts</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+    )
+}
+
+export default Navbar
