2ddb25ee08c8f2b916b1deeb53d82cc593fa0f91
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2020-01-07 07:14:18 -0800

    prepped signup form and started prepping login form for the frontend code

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="nav bg-dark">
-        <li><h1>Crown of Jewels</h1></li>
+        <h1>Crown of Jewels</h1>
         <ul>
             <li>
                 <a href="home.html">Home</a></li>
-            <li><a href="daily-encouragement.html">Daily Encouragent</a></li>
+            <li><a href="daily-encouragement.html">Daily Encouragment</a></li>
             <li><a href="posts.html">Posts</a></li>
             <li><a href="contact.html">Contact</a></li>
         </ul>
