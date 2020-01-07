5cd8b05a74623f9e247e054f2c909699df7a390e
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2020-01-07 15:46:43 -0800

    Uncommented out Signup.js

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,49 +1,49 @@
-// import React, { Fragment, useState } from 'react';
-// // import axios from axios
+import React, { Fragment, useState } from 'react';
+// import axios from axios
 
-// const Signup = () => {
-//     const [formData, setFormData] = useState({
-//         name:'',
-//         email:'',
-//         username: '',
-//         password: '',
-//         password2: '',
-//     });
+const Signup = () => {
+    const [formData, setFormData] = useState({
+        name:'',
+        email:'',
+        username: '',
+        password: '',
+        password2: '',
+    });
 
-//     const { name, email, username, password, password2 } = formData
+    const { name, email, username, password, password2 } = formData
 
-//     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
 
-//     const onSubmit = async e => {
-//         e.preventDefault();
-//         if(password !== password2){
-//             console.log('Passwords do not match')
-//         } else newUser = {
-//             name,
-//             email,
-//             username,
-//             password
-//         }
+    const onSubmit = async e => {
+        e.preventDefault();
+        if(password !== password2){
+            console.log('Passwords do not match')
+        } else newUser = {
+            name,
+            email,
+            username,
+            password
+        }
 
-//         try {
-//             const config = {
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 }
-//             }
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
 
-//             const body = JSON.stringify(newUser);
+            const body = JSON.stringify(newUser);
 
-//             const es = await axios.post();
-//         } catch (err) {
+            const es = await axios.post();
+        } catch (err) {
 
-//         }
-//     }
-//     return <Fragment>
-//         {/* Add Signup Form from Frontend */}
-//         {/* Don't forget to add value and onchange to input tags and "onSubmit={e => onSubmit(e)} to form tag" */}
+        }
+    }
+    return <Fragment>
+        {/* Add Signup Form from Frontend */}
+        {/* Don't forget to add value and onchange to input tags and "onSubmit={e => onSubmit(e)} to form tag" */}
 
-//     </Fragment>
-// }
+    </Fragment>
+}
 
-// export default Signup
+export default Signup
