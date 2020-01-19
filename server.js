40fc8b754e910f0bce40fc54a03850928961085b
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2020-01-18 17:43:58 -0800

    trying to get backend working

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Connect Database
 connectDB();
 
-//Init Middleware
+// Init Middleware
 app.use(express.json({
     extended: false
 }));
@@ -22,13 +22,18 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
+  
+// //     app.get('*', (req, res) => {
+// //       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// //     });
+// //   }
+  
+
 const PORT = process.env.PORT || 5000;
-
-// const server = 
-
 app.listen(PORT, () => 
 console.log(`Server started on port ${PORT}`));
 
-
-
-// module.exports = server;
+module.exports = server;
