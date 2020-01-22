adfa9983b9a61fc007b7a0d0fd6ef6373e53c1af
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2020-01-21 21:00:25 -0800

    worked on getting signup working

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require('express-validator');
 var router = express.Router();
 
 /* GET home page. */
--- server.js
+++ server.js
@@ -6,30 +6,26 @@ const app = express();
 
 // Connect Database
 connectDB();
-
+app.use(cors());
 // Init Middleware
 app.use(express.json({
     extended: false
 }));
 
 app.get('/', (req, res) => res.send('API Running'));
+app.get('/api/auth', (req, res) => {
+    res.send({ express: 'Hello From auth file' });
+  });
 
 // Define Routes
-app.use(cors());
+
 app.options('*', cors());
 app.use('/api/users', require('./routes/api/users'));
 // app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/build'));
-  
-// //     app.get('*', (req, res) => {
-// //       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// //     });
-// //   }
+
   
 
 const PORT = process.env.PORT || 5000;
