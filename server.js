225ab7ddceddd5883c61115f8304be80165cdb08
Vince Oblak <voblak2@gmail.com>
2020-01-28 00:22:54 -0600

    Added files

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 // const path = require('path');
 
 // const auth = require("./routes/api/auth");
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
 // const posts = require("./routes/api/posts")
 
 
@@ -16,24 +16,24 @@ mongoose.connect(
   'mongodb+srv://jhpkinsinger02:Gr8ontheSq@encouraging-moms-ywdax.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true },
   () => console.log('connected to db')
-);
-
+); 
 
 // Connect Database
 // connectDB();
 app.use(cors());
 // Init Middleware
-app.use(express.json({
+app.use(
+  express.json({
     extended: false
-}));
+  })
+);
 
 app.get('/', (req, res) => res.send('API Running'));
 app.get('/api/auth', (req, res) => {
-    res.send({ express: 'Hello From auth file' });
-  });
+  res.send({ express: 'Hello From auth file' });
+});
 
 // Define Routes
-
 
 app.options('*', cors());
 app.use('/api/users', require('./routes/api/users'));
@@ -41,11 +41,7 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-
-  
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => 
-console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // module.exports = server;
