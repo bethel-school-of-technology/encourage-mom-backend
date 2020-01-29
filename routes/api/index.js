3d97ece07103e7bfba8c96f46fe2567f61768a87
unknown <bump.jason@gmail.com>
2020-01-29 12:56:11 -0600

    changes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
@@ -9,16 +8,4 @@ router.get('/', function (req, res, next) {
   });
 });
 
-=======
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
-});
-
->>>>>>> 32a37122b930af4ff9f95552f27614c603198062
 module.exports = router;
